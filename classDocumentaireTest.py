5b51d09297eb1906166dfbf3b735a99b110b8e4c
Mariem_AMZIL <147107786+MAR-AM@users.noreply.github.com>
2023-11-29 23:03:51 +0100

    Documentary Test (●'◡'●)

---
+++ classDocumentaireTest.py
@@ -0,0 +1,12 @@
+from classDocumentaire import Documentaire
+
+#first example
+
+D1 = Documentaire ("ME","222/3/1300")
+D1.Tostring()
+
+#second example 
+
+D2 = Documentaire ("YOU","11/3/4011")
+D2.Tostring()
+
